34a65892f93163fdebee3458d95c66a2ff9363fa
Vijay Bhandari <vijay.bhandari@capgemini.com>
2025-08-27 09:27:55 +0000

    Finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
+from django.utils.html import format_html
+from django import template
+
+register = template.Library()
+
 
 # Create your views here.
+# def index(request):
+#     return render(request, "blog/index.html")
+
 def index(request):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
+
+
+
+@register.filter
+def author_details(author, current_user):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
